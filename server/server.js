3a145d898e49b2a2b4b2663f009ee74f2b8e8d26
Mr. Azharuddin <azharquraishi94@gmail.com>
2025-05-20 20:19:00 +0530

    Done

--- server/server.js
+++ server/server.js
@@ -9,24 +9,13 @@ const app = express();
 
 app.use(cookieParser());
 
-const allowedOrigins = [
-  'https://ecom-1-b7dd.onrender.com', 
-  'http://localhost:3000'             
-];
+// const allowedOrigins = [
+//   'https://ecom-1-b7dd.onrender.com', 
+//   'http://localhost:3000'             
+// ];
 
 
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  exposedHeaders: ['Authorization']
-}));
+app.use(cors());
 
 
 // Mount webhook BEFORE express.json()
